cce08b8f0aac58ef3aea9b0153bbf7ee29405100
simonace <zjjxcai@gmail.com>
2018-07-12 20:42:50 +0800

    Added initial version of PcnCube to model a cube

--- vlsiCadFactory.py
+++ vlsiCadFactory.py
@@ -27,3 +27,58 @@ class PcnFile(object):
         for l in self.rawCubeList:
             if not self._isRawCubeFormatCorrect(l):
                 raise Exception("Cube line format wrong!")
+
+
+class PcnCube(object):
+    def __init__(self, rawCube, varNum):
+        self.varNum = varNum
+        self.noneDcEntryNum = int(rawCube[0])
+        self.noneDcEntries = [int(e) for e in rawCube[1:]]
+        self.getBinaryNotationForm()
+
+    def getBinaryNotationForm(self):
+        self.binaryNotation = PcnCube.convertToBinaryNotationForm(self.noneDcEntries, self.varNum)
+        return self.binaryNotation
+
+    def getNoneDontCareEntryListForm(self):
+        self.noneDcEntries = PcnCube.convertToNoneDontCareEntryListForm(self.binaryNotation, self.varNum)
+        return self.noneDcEntries
+
+    def convertToBinaryNotationForm(noneDcEntries, varNum):
+        binaryNotationList = ['11']*varNum
+        for entry in noneDcEntries:
+            if entry < 0:
+                binaryNotationList[entry*(-1)-1] = '10'
+            else:
+                binaryNotationList[entry-1] = '01'
+        return binaryNotationList
+
+    def convertToNoneDontCareEntryListForm(binaryNotationList, varNum):
+        if varNum != len(binaryNotationList):
+            raise Exception("PCN entry number is inconsistent with the given variable number!")
+        else:
+            noneDcEntries = []
+            for i in range(varNum):
+                if binaryNotationList[i] == '01':
+                    noneDcEntries.append(i+1)
+                elif binaryNotationList[i] == '10':
+                    noneDcEntries.append(-1*(i+1))
+                elif binaryNotationList[i] != '11':
+                    raise Exception("PCN entry must be one of '01', '10' and '11'.")
+            return noneDcEntries
+
+    def _refreshNoneDcEntryNum(self):
+        self.noneDcEntryNum = self.varNum - self._countDontCare()
+
+    def _isAllDontCare(self):
+        return True if self._countDontCare() == self.varNum else False
+
+    def _countDontCare(self):
+        count = 0
+        for e in self.binaryNotation:
+            if e == '11':
+                count = count + 1
+        return count
+
+    def getXn(self, n):
+        return self.binaryNotation[n-1]
